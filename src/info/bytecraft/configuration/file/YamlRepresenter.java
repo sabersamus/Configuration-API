7559977fc66741b84d2c3aedc65bc67cdc9b9d3b
Sabersamus <rcatron10@gmail.com>
2013-08-23 00:46:42 -0700

    Initial commit

---
+++ src/info/bytecraft/configuration/file/YamlRepresenter.java
@@ -0,0 +1,38 @@
+package info.bytecraft.configuration.file;
+
+import info.bytecraft.configuration.ConfigurationSection;
+import info.bytecraft.configuration.serialization.ConfigurationSerializable;
+import info.bytecraft.configuration.serialization.ConfigurationSerialization;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.representer.Representer;
+
+public class YamlRepresenter extends Representer {
+
+    public YamlRepresenter() {
+        this.multiRepresenters.put(ConfigurationSection.class, new RepresentConfigurationSection());
+        this.multiRepresenters.put(ConfigurationSerializable.class, new RepresentConfigurationSerializable());
+    }
+
+    private class RepresentConfigurationSection extends RepresentMap {
+        @Override
+        public Node representData(Object data) {
+            return super.representData(((ConfigurationSection) data).getValues(false));
+        }
+    }
+
+    private class RepresentConfigurationSerializable extends RepresentMap {
+        @Override
+        public Node representData(Object data) {
+            ConfigurationSerializable serializable = (ConfigurationSerializable) data;
+            Map<String, Object> values = new LinkedHashMap<String, Object>();
+            values.put(ConfigurationSerialization.SERIALIZED_TYPE_KEY, ConfigurationSerialization.getAlias(serializable.getClass()));
+            values.putAll(serializable.serialize());
+
+            return super.representData(values);
+        }
+    }
+}
